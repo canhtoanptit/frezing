4d54b66c22b08ae5b2fcec951ba6f231be122a13
nguyen canh toan <v.toannc@vinid.net>
2020-04-26 11:38:49 +0700

    init

---
+++ app/src/main/java/com/canhtoan/freezing/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.canhtoan.freezing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
